cac3826327c379ff4bb71891a71f4ef56dd20ef8
mitkari49 <93242478+mitkari49@users.noreply.github.com>
2024-12-02 17:37:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor (width, height){
+		this.height = height;
+		this.length = width;
+	}
+	getArea () {
+		console.log (this.length*this.height);
+	}
+}
 
-class Square extends Animal {}
+class Square extends Rectangle {
+	constructor (sides) {
+        super(sides, sides);
+		this.sidesOfSquare = sides;
+	}
+	getPerimeter() {
+		console.log(this.sidesOfSquare*4);
+	}
+}
 
+const rectangle = new Rectangle (5,10);
+rectangle.getArea();
+const square = new Square (7);
+square.getPerimeter();
 // Do not change the code below this line
 window.Rectangle = Rectangle;
 window.Square = Square;
